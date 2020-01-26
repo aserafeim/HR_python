a4cd403628db7fb46c0478c67c440ba333ea9666
aserafeim <alex.serafeim@gmail.com>
2020-01-26 03:58:03 +0100

    Add files via upload

---
+++ inputdata.py
@@ -0,0 +1,29 @@
+input_data = {'dt': 0.0001, 'strain_initial': 0,
+              'strain_final': 0.437096511,
+              'strain_rate_initial': 0.402892242,
+            'strain_rate_final': 0.402892242,
+            'temp_initial': 1073,  'temp_final': 1073,
+              'dislocation_density_initial': 1E+11,
+              'pass_interval': 1.084896817,
+              'pass_number': 1}
+fitting_params = {'C_1': 0.8, 'C_2': 0.009, 'C_3': 1, 'C_4': 10, 'C_5': 1.4, 'C_6': 0.0001, 'C_7': 0.00008, 'C_8': 0.9,
+                  'C_9': 1, 'C_10': 1.026E+01}
+
+'Chemical composition'
+chem_comp = {'w_C': 0.077, 'w_Mn': 1.22, 'w_Si': 0.225, 'w_Cr': 0.411, 'w_N': 0.0095, 'w_Nb': 0, 'w_Ti': 0}
+
+'Constants'
+shear_mod_G0 = 8.10E+10
+melting_temp_TmG = 1709
+rho_const = 3.14E+13
+grain_size_init = 5.00E-04
+M = 3
+alpha = 0.1
+C0 = 750
+activation_energy_static_rx = 92100
+uni_gas_const = 8.314
+'''% Stefan Boltzmann constant in W/(m^2.K^4)'''
+K_b = 1.38E-23
+'''% Attack frequency in 1/s'''
+nu_a = 7600000000
+n_x = 0.06
